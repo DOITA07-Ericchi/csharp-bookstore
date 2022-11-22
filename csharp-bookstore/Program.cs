1a0d54a9000f5ef3e8b1f359e72a9a37219a5ad2
EricchiYukia <git@ericchi.moe>
2022-11-22 16:04:30 +0100

    Variabili inserite

--- csharp-bookstore/Program.cs
+++ csharp-bookstore/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Tesht");
+string titolo = "Strawberry Panic: The Complete Novel Collection";
+string autore = "Sakurako Kimino";
+string isbn = "978-1934876992";
+ushort pagine = 470;
+float peso = 0.413f;
+float altezza = 12.88f;
+float larghezza = 4.36f;
+float profondita = 18.24f;
+float valutazione = 4.4f;
+ulong recensioni = 44;
+bool kindle = false;
+bool copFlessibile = true;
+string url = "https://www.amazon.it/Strawberry-Panic-Complete-Novel-Collection/dp/1934876992/";
+
+Console.WriteLine()
